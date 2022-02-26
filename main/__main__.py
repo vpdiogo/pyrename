ccfe841f0cdc2b4038231a383751a1eddc81c060
Vitor <vitorpdiogo@gmail.com>
2022-02-26 02:09:11 -0300

    new structure

--- main/__main__.py
+++ main/__main__.py
@@ -1,5 +1,10 @@
-if __name__ == "__main__":
-    import sys
+import sys
+
+
+def main():
     old_file = str(sys.argv[1])
     new_file = str(sys.argv[2])
     key = str(sys.argv[3])
+
+if __name__ == "__main__":
+    main()
